f92ef5f4c5d2425ca71725c9110802ee89025f11
ioannakok <ioannakok@hotmail.com>
2018-06-04 23:30:59 +0100

    Make instance variable amount of Dollar class private

--- tests/Tests.java
+++ tests/Tests.java
@@ -9,10 +9,8 @@ public class Tests {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(10, five.times(2));
+        assertEquals(15, five.times(3));
     }
 
     @Test
