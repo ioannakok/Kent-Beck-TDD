a41ea133bfb2874b207b966e860048023292366a
ioannakok <ioannakok@hotmail.com>
2018-06-05 00:18:46 +0100

    Correct Dollar and Franc multiplication tests

--- tests/Tests.java
+++ tests/Tests.java
@@ -9,8 +9,8 @@ public class Tests {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        assertEquals(10, five.times(2));
-        assertEquals(15, five.times(3));
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
@@ -22,7 +22,7 @@ public class Tests {
     @Test
     public void testFrancMultiplication() {
         Franc five = new Franc(5);
-        assertEquals(10, five.times(2));
-        assertEquals(15, five.times(3));
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
